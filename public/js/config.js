fbb984bc57e8213c933e8c74563e64bcc379acc9
Latisha19 <85289926+Latisha19@users.noreply.github.com>
2022-02-27 17:05:47 +0800

    Add files via upload

---
+++ public/js/config.js
@@ -0,0 +1,7 @@
+turnConfig = {
+    iceServers: [{urls: ["stun:hk-turn1.xirsys.com"]}, {
+        username: "7DhzFpj6rYfqBQ7iDPRSxyF_gdYXzJuYqc69d4T3usjtVazf_iD22by4rENPokorAAAAAGIbPJtsaW5zaGFu",
+        credential: "12ae9df2-97ab-11ec-86de-0242ac120004",
+        urls: ["turn:hk-turn1.xirsys.com:80?transport=udp", "turn:hk-turn1.xirsys.com:3478?transport=udp", "turn:hk-turn1.xirsys.com:80?transport=tcp", "turn:hk-turn1.xirsys.com:3478?transport=tcp", "turns:hk-turn1.xirsys.com:443?transport=tcp", "turns:hk-turn1.xirsys.com:5349?transport=tcp"]
+    }]
+}
